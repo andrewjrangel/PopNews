edf811f24ab785ab7f861e4d20f4dfb7be408029
Andrew Rangel <andrewjrangel@gmail.com>
2015-02-06 17:53:15 -0600

    Initial Commit

---
+++ PopNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PopNews
+//
+//  Created by AndrewJRangel on 2/6/15.
+//  Copyright (c) 2015 AndrewJRangel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
